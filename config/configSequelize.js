c0ef038d0222453d0e463115f6d14c6dbe6b0093
Camis Candido <camilacvc@hotmail.com>
2022-03-29 19:54:36 -0300

    começando as models

---
+++ config/configSequelize.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "username": "root",
+  "password": null,
+  "database": "tocaqui",
+  "host": "127.0.0.1",
+  "dialect": "mariadb"
+}
---
+++ models/musico.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Musico extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Musico.init({
+    nome: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Musico',
+    tableName: 'Musicos', 
+    freezeTableName: true
+  });
+  return Musico;
+};
