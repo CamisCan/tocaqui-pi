99e7e90ecb70032095c6d5f163174ed0e2100d17
Camis Candido <camilacvc@hotmail.com>
2022-03-29 21:36:44 -0300

    models: mensagem, musico e usuario

--- models/mensagem.js
+++ models/mensagem.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Usuario extends Model {
+  class Mensagem extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,15 +13,14 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Usuario.init({
-    foto_perfil: DataTypes.STRING,
-    email: DataTypes.STRING,
-    senha: DataTypes.STRING
+  Mensagem.init({
+    mensagem: DataTypes.STRING,
+    data: DataTypes.DATE
   }, {
     sequelize,
-    modelName: 'Usuario',
-    tableName: 'usuarios',
+    modelName: 'Mensagem',
+    tableName: 'mensagens',
     timestamps: false
   });
-  return Usuario;
+  return Mensagem;
 };
